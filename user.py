d8d7057d97aa1688eb5ae2d21149f22afd0223c1
annechege <annechege780@gmail.com>
2021-06-15 13:58:39 +0300

    calling in functions of the init

--- user.py
+++ user.py
@@ -6,4 +6,9 @@ class User:
 
   user_list = [] # creates an empty user list
 
-    
+        def __init__(self,first_name,last_name,number,email):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = number
+        self.email = email
