590f349624b3c245f0af84019727b268f5ddf0df
annechege <annechege780@gmail.com>
2021-06-15 14:55:50 +0300

    functions that shows all the saved user

--- run.py
+++ run.py
@@ -10,3 +10,39 @@ def create_user(fname,lname,phone,email):
   '''
   new_user = User(fname,lname,phone,email)
   return new_user
+def save_user(user):
+  '''
+  Function to save user
+  '''
+  user.save_user()
+
+def del_user(user):
+  '''
+  Function to delete a user
+  '''
+  user.delete_user()
+
+def copy_email(user,number):
+  '''
+  Function to delete a user
+  '''
+  user.copy_email(number)
+
+def find_user(number):
+  '''
+  Function that finds a user by number and returns the user
+  '''
+  return User.find_by_number(number)
+
+def check_existing_user(number):
+  '''
+  Function that check if user exists with number and returns a Boolean
+  '''
+  return User.user_exist(number)
+
+def display_users():
+  '''
+  Function that returns all the saved users
+  '''
+  return User.display_users()
+
