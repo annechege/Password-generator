07faedcfd939afec85b288413f78785e83dc2f43
annechege <annechege780@gmail.com>
2021-06-15 14:09:43 +0300

    add the user exit

--- user.py
+++ user.py
@@ -6,26 +6,29 @@ class User:
 
   user_list = [] # creates an empty user list
 
-        def __init__(self,first_name,last_name,number,email):
+  def __init__(self,first_name,last_name,number,email):
 
-        self.first_name = first_name
-        self.last_name = last_name
-        self.phone_number = number
-        self.email = email
-        def save_user(self):
+    self.first_name = first_name
+    self.last_name = last_name
+    self.phone_number = number
+    self.email = email
+
+  def save_user(self):
     '''
     save_user method saves user object into the user_list
     '''
 
     User.user_list.append(self)
-    def delete_user(self):
+
+  def delete_user(self):
     '''
     delete_user method deletes a saved user from the user_list
     '''
 
     User.user_list.remove(self)
-    @classmethod
-def find_by_number(cls,number):
+
+  @classmethod
+  def find_by_number(cls,number):
     '''
     Method that takes in a number and returns a user that matches that number.
 
@@ -35,9 +38,6 @@ def find_by_number(cls,number):
       User  that matches the number.
     '''
     for user in cls.user_list:
-      if user.phone_number == number:
-        return user
- for user in cls.user_list:
       if user.phone_number == number:
         return user
 
@@ -55,7 +55,8 @@ def find_by_number(cls,number):
         return True
 
     return False
-     @classmethod
+
+  @classmethod
   def display_users(cls):
     '''
     method that returns the user list
