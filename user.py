a11b95744b1ea6ac1d20803450290b088d702a99
annechege <annechege780@gmail.com>
2021-06-15 14:01:51 +0300

    add the class method

--- user.py
+++ user.py
@@ -12,3 +12,28 @@ class User:
         self.last_name = last_name
         self.phone_number = number
         self.email = email
+        def save_user(self):
+    '''
+    save_user method saves user object into the user_list
+    '''
+
+    User.user_list.append(self)
+    def delete_user(self):
+    '''
+    delete_user method deletes a saved user from the user_list
+    '''
+
+    User.user_list.remove(self)
+    @classmethod
+def find_by_number(cls,number):
+    '''
+    Method that takes in a number and returns a user that matches that number.
+
+    Args:
+      number:Phone number to search for
+    Returns:
+      User  that matches the number.
+    '''
+    for user in cls.user_list:
+      if user.phone_number == number:
+        return user
