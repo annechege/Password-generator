c677f9d6894ab427663a20dc47f42b523ad39a26
annechege <annechege780@gmail.com>
2021-06-15 14:15:03 +0300

    saves credential methods

--- credential.py
+++ credential.py
@@ -9,3 +9,16 @@ class Credential:
     self.credential_name = credential_name
     self.password = password
     self.number = number
+    def save_credential(self):
+    '''
+    save_credential method saves credential object into the credential_list
+    '''
+
+    Credential.credential_list.append(self)
+
+  def delete_credential(self):
+    '''
+    delete_credential method deletes a saved credential from the credential_list
+    '''
+
+    Credential.credential_list.remove(self)
